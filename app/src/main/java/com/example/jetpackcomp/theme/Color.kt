54b25d6f931e2eae17cbee45dae514f24dd0bfef
deepakkanyan <dpk.ror7@gmail.com>
2022-12-24 21:17:55 -0500

    ComposeUI with Clean Architecture

--- app/src/main/java/com/example/jetpackcomp/theme/Color.kt
+++ app/src/main/java/com/example/jetpackcomp/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.example.jetpackcomp.ui.theme
+package com.example.jetpackcomp.theme
 
 import androidx.compose.ui.graphics.Color
 
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val Grey40 = Color(0xFFF0EEEE)
+val Red40 = Color(0xFFFF5722)
+val RedDark = Color(0xFFD32F2F)
